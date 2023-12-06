c62806389adb1e939f3348cb23247b46022497ae
godwinekh <godwinekh@gmail.com>
2023-12-06 10:07:01 +0100

    added layout for about-us page

--- src/app/about-us/Meet.tsx
+++ src/app/about-us/Meet.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import Xander from "@/images/xander-uncut.jpg";
+
 export default function Meet() {
   return (
-    <div>
+    <div className="py-24 w-full layout relative">
       <h2>Meet the founder</h2>
+
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2 relative">
+          <Image
+            alt="Eke Alexander"
+            src={Xander}
+            quality={100}
+            placeholder="blur"
+            className="w-full h-full"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+          <div className="absolute bottom-0 w-full px-5 bg-amber-200 text-center">
+            <h5 className="text-gray-800">Eke Alexander</h5>
+          </div>
+        </div>
+
+        <div className="w-full md:w-3/4">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima ab
+            iure error voluptate autem nostrum architecto dicta ratione ut
+            nesciunt reprehenderit, officia et tempore repudiandae, reiciendis
+            laudantium odit animi in.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima ab
+            iure error voluptate autem nostrum architecto dicta ratione ut
+            nesciunt reprehenderit, officia et tempore repudiandae, reiciendis
+            laudantium odit animi in.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,13 +2,18 @@ import { TopBar } from "@/app/header/TopBar";
 import { Fragment } from "react";
 import Structure from "../structure";
 import BottomBar from "../footer/BottomBar";
+import Meet from "./Meet";
+import Story from "./Story";
+import { Connect } from "../home/Connect";
 
 export default function AboutUs() {
   return (
     <Fragment>
       <TopBar />
       <Structure>
-        <div></div>
+        <Story />
+        <Meet />
+        <Connect />
       </Structure>
       <BottomBar />
     </Fragment>
